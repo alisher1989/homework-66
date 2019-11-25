b7bf044d8fe1e4958d711dd157a2f92f20030c7f
alisher <shera.rashiduulu@gmail.com>
2019-11-25 16:47:04 +0200

    deleted no need files

--- main.js
+++
@@ -1,9 +0,0 @@
-// var div1 = document.getElementById('div-1');
-// console.log(div1);
-function onButtonClick() {
-    let input = document.getElementById('my-input');
-    input.value = parseInt(input.value) + 1;
-}
-
-var button = document.getElementsByTagName('button')[0];
-button.onclick = onButtonClick;
