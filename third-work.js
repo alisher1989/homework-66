65e56d59cd83cf65eb0e1f5ed5baffd9db981fcd
alisher <shera.rashiduulu@gmail.com>
2019-11-25 16:58:34 +0200

    Created index html pages for each work

--- third-work.js
+++ third-work.js
@@ -1,12 +1,12 @@
 var toAdd = document.createDocumentFragment();
 for(var i=1; i < 6; i++){
    var newDiv = document.createElement('div');
-   newDiv.innerText = 'element'+' '+i;
+   newDiv.innerText = 'Element'+' '+i;
    newDiv.className = 'element';
    toAdd.appendChild(newDiv);
    console.log(newDiv)
 }
-var parentDiv = document.getElementsByClassName('container')[0];
+var parentDiv = document.getElementById('container');
 parentDiv.appendChild(toAdd);
 
 
